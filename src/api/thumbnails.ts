fa4ae121838ec73f89180e9777f82793284a83f6
ramZenit <a.mb@outlook.it>
2025-12-17 11:43:34 +0100

    implement thumbnails multipart upload

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,9 +1,11 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
+
+const MAX_UPLOAD_SIZE = 10 << 20; // 10 MB
 
 type Thumbnail = {
   data: ArrayBuffer;
@@ -47,7 +49,35 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
 
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
-  // TODO: implement the upload here
+  const formData = await req.formData();
+  const file = formData.get("thumbnail");
 
-  return respondWithJSON(200, null);
+  if (!(file instanceof File)) {
+    throw new BadRequestError("No thumbnail file provided");
+  }
+  if (file.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError("Thumbnail file is too large");
+  }
+
+  const mediaType = file.type;
+  const buffer = await file.arrayBuffer();
+
+  const video = getVideo(cfg.db, videoId);
+  if (!video) {
+    throw new NotFoundError("Couldn't find video");
+  }
+
+  if (video.userID !== userID) {
+    throw new UserForbiddenError(
+      "You do not have permission to modify this video"
+    );
+  }
+
+  videoThumbnails.set(videoId, { data: buffer, mediaType: mediaType });
+
+  const thumbnailURL = `http://localhost:${cfg.port}/api/thumbnails/${videoId}`;
+  video.thumbnailURL = thumbnailURL;
+  updateVideo(cfg.db, video);
+
+  return respondWithJSON(200, video);
 }
